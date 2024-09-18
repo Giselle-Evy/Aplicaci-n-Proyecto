5d60775b52624f5b335afd171eaef19a3053ba0a
Giselle-Evy <Gislyngonzalez077@gmail.com>
2024-09-17 23:07:13 -0600

    Creacion de la seccion de imagenes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import Navbar from './Componentes/Navbar.jsx'; // Importa el Navbar
+import Images from './Componentes/Images.jsx'; // Importa las imagenes
 import './App.css'; // Importa los estilos
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <section className="Coleccion">
+        <h1>Nueva Colección de Ropa Primavera 2024</h1>
+        <p>"Descubre los colores y estilos que definirán esta temporada."</p>
+        <Images />
+      </section>
+      
     </div>
   );
 }
---
+++ src/Componentes/Images.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Images = () => {
+  return (
+    <div className="images-gallery">
+      <img src="/imagenes/Modelo1.jpg"  alt="ropa1" />
+      <img src="/imagenes/Modelo2.jpg"  alt="ropa2" />
+      <img src="/imagenes/Modelo3.jpg"  alt="ropa3" />
+      <img src="/imagenes/Modelo4.jpg"  alt="ropa4" />
+      <img src="/imagenes/Modelo5.jpg"  alt="ropa5" />
+    </div>
+  );
+};
+
+export default Images;
